f6be51acae5c79af767ca6b73922dcea7c550484
sanyuktadeshmukh <karanskhedkar@gmail.com>
2023-08-28 13:09:24 +0530

    .

--- sql_connection.py
+++ sql_connection.py
@@ -8,7 +8,12 @@ def get_sql_connection():
   global __cnx
 
   if __cnx is None:
-    __cnx = mysql.connector.connect(user='root', password='9297', database='store')
+    __cnx  = mysql.connector.connect(
+        host='containers-us-west-136.railway.app',
+        user='root',
+        password='ODQdfkMDBAdqParIUm9d',
+        database='store'
+    )
 
   return __cnx
 
